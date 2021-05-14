d21b3d224fb3ed5baf3e0e0108cc85a7d8e3b0fd
William Lucht <aaronlucht@gmail.com>
2021-05-13 21:09:19 -0700

    minor fix to Buttons lay

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,9 +7,9 @@ function Buttons() {
         <div class="container">
           <h1 class="display-4">Welcome to your Employee Dashboard</h1>
           <p class="lead">
-            Please use the buttons below to filter employees by{" "}
-            <strong>Name</strong>, <strong>Email</strong>,<strong>Phone</strong>
-            , and <strong>Birthday</strong>
+            Please use the buttons below to filter employees by
+            <strong> Name</strong>, <strong>Email</strong>,{" "}
+            <strong>Phone</strong>, and <strong>Birthday</strong>
           </p>
           <button type="button" class="btn btn-secondary btn-lg ml-2">
             Name
